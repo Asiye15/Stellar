8d9a7203bdf4f5df04ae07b245d8e529cc5d51be
Kursaitova15 <kursaitova15@gmail.com>
2020-05-18 15:30:29 +0300

    Add logo

--- main/pages/PHome/index.js
+++ main/pages/PHome/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { observer } from 'startupjs'
 import { ScrollView, View } from 'react-native'
-import { Content, Footer } from 'components'
+import { Content, Footer, Logo } from 'components'
 import './index.styl'
 
 export default observer(function PHome () {
   return pug`
     ScrollView.root
       View.main
+        Logo
         Content
         Footer
           
